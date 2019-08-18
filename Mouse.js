40d62bc2132f49e24fa8d6e709ec84a7ab3bff6e
PhamDucLuong <luongphama1@gmail.com>
2019-08-18 12:44:52 +0700

    Add property death and function die for Mouse

--- Mouse.js
+++ Mouse.js
@@ -1,6 +1,10 @@
 function Mouse(name,weight){
     this.name = name;
     this.weight = weight;
+    this.dead = false;
+}
+Mouse.prototype.die = function(){
+    this.dead = true;
 }
 Mouse.prototype.sayHello = function(){
     return "Hello every one";
