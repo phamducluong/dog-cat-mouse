798a087d04d5d21b1d63fc2610c89a13d132490a
PhamDucLuong <luongphama1@gmail.com>
2019-08-18 16:42:41 +0700

    let Dog eat Cat

--- Dog.js
+++ Dog.js
@@ -1,7 +1,11 @@
 const chalk  = require('chalk');
 function Dog (name)
 {
+    this.stomach = [];
     this.name = name;
+}
+Dog.prototype.eat = function(cat){
+    this.stomach.push(cat);
 }
 Dog.prototype.say = function(){
     console.log('My name is '+ chalk.blue(this.name));
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const Dog = require('./Dog');
+const Cat =require('./Cat');
+const tempCat = new Cat('Temp');
 const tempDog = new Dog('Duy');
-tempDog.say();
+tempDog.eat(tempCat);
+console.log(tempDog);
